74032faf61dc9a0252314f0b12d88175cbf14eff
doctor <fd5@MacBook-Air-2022.local>
2023-12-15 14:38:22 +0700

    ex1

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,28 @@
-class EmailNotification { }
+class EmailNotification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`send email to ${this.receiver}`);
+  }
+}
 
-class SMSNotification { }
+class SMSNotification {
+  constructor(notificationId, createdTime, content, phoneNumber) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.phoneNumber = phoneNumber;
+  }
+  send() {
+    console.log(`send SMS to ${this.phoneNumber}`);
+  }
+}
+
+let email1 = new EmailNotification(1, "15:12", "Hello", "hi");
+let sms1 = new SMSNotification(2, "15:12", "Hello", "0811111111");
+console.log(email1);
+console;
